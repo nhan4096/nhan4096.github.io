d737300a07900278a29d418aa4ef59cf31dcd9ff
nhan4096 <142195427+nhan4096@users.noreply.github.com>
2025-04-05 13:05:50 +0700

    udate to tro-choi-roi-la

--- tro-choi-roi-la/script.js
+++ tro-choi-roi-la/script.js
@@ -4,7 +4,11 @@ async function startGame() {
         alert("Còn những tham số chưa được nhập vào!");
         return;
     }
-    let s = generateString($('#len').val(), $('#max-count').val(), $('#allow-negative').is(":checked"));
+    s = ['', 0]
+    while (Math.abs($('#len').val()-s[0].length) > $('#len').val()*0.2) {
+        s = generateString($('#len').val(), $('#max-count').val(), $('#allow-negative').is(":checked"));
+    }
+    // console.log(s[0], s[1], s[0].length)
     $('.start-div').hide();
     $('.game-div').show();
     for (let i=0; i<s[0].length; i++) {
@@ -30,15 +34,28 @@ async function startGame() {
     $('#status').show()
 
     let count = $('#final-count').val();
-    if (count == s[1]) {
+    if (count === s[1]) {
+        new Audio('fart-achievement.mp3').play()
         $('#status').text("CHEATeR INSSPeCT ELEMENTTTTTTT");
     }
     else {
+        new Audio('chicken-dance.mp3').play()
         $('#status').text("NGU BÒ ĐÁP ÁN ĐÚNG LÀ " + s[1] + " LẦN ỈA");
     }
 
     $('.command').text("Kết thúc");
     $('#return-btn').show();
+}
+
+function factorial(n) {
+    if (n <= 8) {
+        return [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n]
+    }
+    let prod = 1
+    for (let i=1; i<=n; i++) {
+        prod *= i
+    }
+    return prod
 }
 
 function returnHome() {
@@ -55,7 +72,6 @@ function sleep(ms) {
 }
 
 function generateString(len, maxCount, allowNegatives) {
-    console.log(allowNegatives)
     let count = 0;
     let string = "";
     while (true) {
@@ -70,21 +86,42 @@ function generateString(len, maxCount, allowNegatives) {
         }
         else {
             let rngSeconds = Math.random();
-            if (rngSeconds < 1/3 && count % 2 == 0) {
+            if (rngSeconds < 1/6 && count % 2 == 0) {
                 string += "M";
                 count = Number(count/2);
             }
-            else if (rngSeconds < 2/3) {
+            else if (rngSeconds < 2/6) {
                 string += "W";
                 count *= 2;
             }
-            else {
+            else if (rngSeconds < 3/6) {
                 string += "X";
                 count = 0;
             }
+            else if (rngSeconds < 4/6 && Number(Math.sqrt(count))*Number(Math.sqrt(count)) == count) {
+                string += "D";
+                count = Number(Math.sqrt(count));
+            }
+            else if (rngSeconds < 5/6) {
+                string += "A";
+                count *= count;
+            }
+            else {
+                string += "T";
+                // count = factorial(count);
+                if (factorial(count) <= 250) {
+                    count = factorial(count)
+                }
+                else {
+                    continue
+                }
+            }
         }
-        let finished = Math.random() < 1/len ? true : false;
-        if (finished && count <= maxCount && string.length > 5 && string[string.length-1] != "X") {
+        if (string.length > len*1.2) {
+            return ['', -1]
+        }
+        let finished = Math.random() < 3.5/len ? true : false;
+        if (finished && count <= maxCount && string.length > len/2 && string[string.length-1] != "X") {
             return [string, count];
         }
     }
