1a99d786f1c7e8d8a814d7352cd723b62ab3c314
nhan4096 <142195427+nhan4096@users.noreply.github.com>
2025-05-10 08:32:31 +0700

    some bug fixes + sound

--- puzzles/script.js
+++ puzzles/script.js
@@ -27,12 +27,14 @@ function loadPuzzle(e) {
             document.getElementById("puzzle-text").innerHTML = puzzleText;
             document.getElementById("puzzle-answer").value = "";
             document.getElementById("puzzle-answer").disabled = false;
+            document.getElementById("submit-answer").disabled = false;
             document.getElementById("popup-dialog").style.backgroundColor = "#1a1a1a";
             document.getElementById("puzzle-date").innerHTML = `${month} ${day + (day % 10 == 1 ? "st" : (day % 10 == 2 ? "nd" : (day % 10 == 3 ? "rd" : "th")))}, ${year}`;
 
             if (localStorage.getItem("puzzle-" + doc.id) == "solved") {
                 document.getElementById("puzzle-answer").value = localStorage.getItem("answer-" + doc.id);
                 document.getElementById("puzzle-answer").disabled = true;
+                document.getElementById("submit-answer").disabled = true;
                 document.getElementById("popup-dialog").style.backgroundColor = "#4caf50";
                 document.getElementById(puzzleId).classList.add("solved");
             }
@@ -68,11 +70,20 @@ async function checkAnswer() {
             if (doc.id == currPuzzle) {
                 const isCorrect = await correct(answer, doc);
                 if (isCorrect) {
+                    const puzzleId = doc.id;
                     localStorage.setItem("puzzle-" + currPuzzle, "solved");
                     localStorage.setItem("answer-" + currPuzzle, answer);
                     document.getElementById("popup-dialog").style.backgroundColor = "#4caf50";
                     document.getElementById("puzzle-answer").disabled = true;
-                };
+                    document.getElementById(puzzleId).classList.add("solved");
+                    document.getElementById("submit-answer").disabled = true;
+                    solvedPuzzles++;
+                    document.getElementById("solved-count").innerHTML = `Solved: ${solvedPuzzles} / ${puzzleList.size}`;
+                    new Audio("correct.mp3").play();
+                }
+                else {
+                    new Audio("wrong.mp3").play();
+                }
             };
         };
     };
@@ -99,22 +110,31 @@ const puzzleCollection = collection(db, "puzzles");
 const puzzleList = await getDocs(puzzleCollection);
 const puzzleHTML = document.getElementById("puzzle-list");
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+var solvedPuzzles = 0;
 
 puzzleList.forEach((doc) => {
     let data = doc.data();
     let puzzleItem = document.createElement("div");
+    let date = new Date(data.date.seconds * 1000 + data.date.nanoseconds / 1000000);
+    let year = date.getFullYear();
+    let month = months[date.getMonth()];
+    let day = date.getDate();
+
     puzzleItem.className = "puzzle";
     puzzleItem.id = doc.id;
     puzzleItem.innerHTML = `
         <h2 class="center bold no-margin" id="${doc.id}">${data.name}</h2>
+        <p class="no-margin italics">${month} ${day + (day % 10 == 1 ? "st" : (day % 10 == 2 ? "nd" : (day % 10 == 3 ? "rd" : "th")))}, ${year}</p>
         <img class="img-puzzle" src="${data.img}" alt="${data.name}" id="${doc.id}">
     `;
     if (localStorage.getItem("puzzle-" + doc.id) == "solved") {
         puzzleItem.classList.add("solved");
+        solvedPuzzles++;
     }
     puzzleHTML.appendChild(puzzleItem);
     puzzleItem.addEventListener("click", loadPuzzle);
 });
 
+document.getElementById("solved-count").innerHTML = `Solved: ${solvedPuzzles} / ${puzzleList.size}`;
 document.getElementById("close-popup").addEventListener("click", closePopup);
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
