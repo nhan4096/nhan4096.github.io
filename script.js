464a7fc85673d0ac6ddfbb28d92fb84453b412c9
nhan4096 <dothanhnhan.1465@gmail.com>
2025-08-08 14:50:57 +0700

    next icare udate!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function gotoPage(p) {
-    window.location.href = p;
+let buttons = document.querySelectorAll('.sub-button');
+let buttonsContainer = document.getElementById('buttons');
+let width = buttonsContainer.offsetWidth;
+let selected = -1;
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.width = width / buttons.length + 'px';
+    buttons[i].addEventListener('click', function (e) {
+        if (selected !== -1) {
+            buttons[selected].classList.remove('selected');
+        }
+        selected = i;
+        buttons[i].classList.add('selected')
+
+        for (let j = -1; j < buttons.length; j++) {
+            document.getElementById(`text-${j}`).hidden = true;
+        }
+        document.getElementById(`text-${i}`).hidden = false;
+    })
 }
