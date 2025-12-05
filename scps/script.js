a525b03c46d342063b72176469238f982a477494
nhan4096 <dothanhnhan.1465@gmail.com>
2025-12-05 22:26:09 +0700

    SCPS!!!!!!!!!!!!!!!!! first update in a while!!!!!

---
+++ scps/script.js
@@ -0,0 +1,21 @@
+let buttons = document.querySelectorAll('.sub-button');
+let buttonsContainer = document.getElementById('buttons');
+let width = buttonsContainer.offsetWidth;
+let selected = -1;
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.width = width / Math.min(buttons.length, 5) + 'px';
+    buttons[i].addEventListener('click', function (e) {
+        if (selected !== -1) {
+            buttons[selected].classList.remove('selected');
+        }
+        selected = i;
+        buttons[i].classList.add('selected')
+
+        for (let j = -1; j < buttons.length; j++) {
+            console.log(selected);
+            document.getElementById(`text-${j}`).hidden = true;
+        }
+        document.getElementById(`text-${i}`).hidden = false;
+    })
+}
